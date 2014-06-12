72690811819805f8bc36e07a4c83d89d814f163e
Le Wang <le.wang@agworld.com.au>
2014-06-12 10:41:12 -0400

    Use github token instead of u/p, refactor into methods.
    - Support our workflow of exporting as CSV and then editing, adding
      "epic" column and importing into pivotal.

--- gh2ptissues.rb
+++ gh2ptissues.rb
@@ -2,99 +2,186 @@
 
 # Description:  Migrates GitHub Issues to Pivotal Tracker.
 
-GITHUB_ORG = 'APX'
-GITHUB_USER = 'shyam-habarakada'
-GITHUB_REPO = 'APX/benchpress-server'
-GITHUB_LOGIN = 'shyam-habarakada'
-PIVOTAL_PROJECT_ID = 836893
+GITHUB_REPO = 'shifthealthparadigms/tickit-app'
+PIVOTAL_PROJECT_ID = 1093508
 PIVOTAL_PROJECT_USE_SSL = true
 
-GITHUB_PASSWORD = ENV['GITHUB_PASSWORD']
+GITHUB_TOKEN = ENV['GITHUB_TOKEN']
 PIVOTAL_TOKEN = ENV['PIVOTAL_TOKEN'].to_s unless ENV['PIVOTAL_TOKEN'].nil?
 
 require 'rubygems'
 require 'octokit'
 require 'pivotal-tracker'
 require 'json'
+require 'debugger'
+require 'csv'
+require 'rest_client'
 
-# uncomment to debug
-# require 'net-http-spy'
-# puts ENV['GITHUB_PASSWORD']
-# puts ENV['PIVOTAL_TOKEN']
+def get_pivotal_epics_hash
+  @epic_hash ||= -> {
+    epic_url = 'https://www.pivotaltracker.com/services/v5/projects/' + PIVOTAL_PROJECT_ID.to_s() + '/epics' + '?token=' + PIVOTAL_TOKEN
 
-dry_run = true
+    res = RestClient.get(epic_url, {:accept => :json})
+    res_hash = JSON.parse(res)
 
-begin
+    Hash[res_hash.map{|r| [r['name'], r["label"]["name"]]}]
+  }[]
+end
 
-  PivotalTracker::Client.token = PIVOTAL_TOKEN
-  PivotalTracker::Client.use_ssl = PIVOTAL_PROJECT_USE_SSL
+def get_csv_arr(file)
+  csv = CSV.new(File.open(file).read, headers: true)
+  csv.to_a.map{|r|r.to_h}
+end
 
-  pivotal_project = nil
+def fix_up_epics(rows)
+  epic_hash = get_pivotal_epics_hash
+  epic_reverse_hash = Hash[epic_hash.map(&:reverse)]
+  rows.map do |row|
+    epic = row["epic"]
+    if epic_reverse_hash["epic"]
+    elsif epic_hash[epic]
+      row["epic"] = epic_hash[epic]
+    else
+      raise "can't find epic: #{epic}"
+    end
+    row
+  end
 
-  pivotal_project = PivotalTracker::Project.find(PIVOTAL_PROJECT_ID)
+end
 
-  github = Octokit::Client.new(:login => GITHUB_LOGIN, :password => GITHUB_PASSWORD)
+def github_issues_to_arr
 
-  issues_filter = 'bug' # update filter as appropriate
-
-  story_type = 'bug' # 'bug', 'feature', 'chore', 'release'. Omitting makes it a feature.
-
-  story_current_state = 'unscheduled' # 'unscheduled', 'started', 'accepted', 'delivered', 'finished', 'unscheduled'.
-                                      # 'unstarted' puts it in 'Current' if Commit Mode is on; 'Backlog' if Auto Mode is on.
-                                      # Omitting puts it in the Icebox.
-
-  total_issues = 0 
+  issues_filter = 'pivotal' # update filter as appropriate
+  total_issues = 0
 
   page_issues = 1
-  issues = github.list_issues(GITHUB_REPO, { :page => page_issues, :labels => issues_filter } )
 
+  issues = @github.list_issues(GITHUB_REPO, { :page => page_issues, :labels => issues_filter } )
+  arr = []
   while issues.count > 0
 
     issues.each do |issue|
       total_issues += 1
-      comments = github.issue_comments(GITHUB_REPO, issue.number)
-      
-      labels = 'github-import'
-      issue.labels.each do |l|
-        labels += ",#{l.name}"
-      end
 
-      puts "issue: #{issue.number} #{issue.title}, with #{comments.count} comments"
+      labels = issue.labels.map(&:name).reject {|n| n =~ /#{issues_filter}/i}
 
-      unless dry_run
-        story = pivotal_project.stories.create(
-          :name => issue.title,
-          :description => issue.body,
-          :created_at => issue.created_at,
-          :labels => labels,
-          :story_type => story_type,     
-          :current_state => story_current_state
-        )
-
-        story.notes.create(
-          text: "Migrated from #{issue.html_url}",
-        )
-
-        comments.each do |comment|
-          story.notes.create(
-            text: comment.body.gsub(/\r\n\r\n/, "\n\n"),
-            author: comment.user.login,
-            noted_at: comment.created_at
-          )
-        end
-        
-        github.add_comment(GITHUB_REPO, issue.number, "Migrated to pivotal tracker #{story.url}")
-        github.close_issue(GITHUB_REPO, issue.number)
-      end
+      arr << {'id' => issue.number,
+        'title' => issue.title,
+        'labels' => labels,
+        'description' => issue.body,
+        'url' =>  issue.url
+      }
     end
 
     page_issues += 1
-    issues = github.list_issues(GITHUB_REPO, { :page => page_issues, :labels => issues_filter } )
+    issues = @github.list_issues(GITHUB_REPO, { :page => page_issues, :labels => issues_filter } )
   end
 
-  puts "\n== processed #{total_issues} issues"
+  arr
+end
 
-rescue StandardError => se
-  puts se.message
-  exit 1
+def github_issues_to_csv(file)
+  arr = github_issues_to_arr
+
+  CSV.open(file, "w") do |csv|
+    keys = arr.first.keys
+    csv << keys
+    arr.each do |row|
+      csv << keys.map{|k| row[k]}
+    end
+  end
+end
+
+def pivotal_import_stories(issues)
+
+  begin
+
+    story_type = 'feature' # 'bug', 'feature', 'chore', 'release'. Omitting makes it a feature.
+
+    story_current_state = 'unscheduled' # 'unscheduled', 'started', 'accepted', 'delivered', 'finished', 'unscheduled'.
+    # 'unstarted' puts it in 'Current' if Commit Mode is on; 'Backlog' if Auto Mode is on.
+    # Omitting puts it in the Icebox.
+
+    issues.each do |issue|
+      ### LE::skip comments
+      # comments = github.issue_comments(GITHUB_REPO, issue.number)
+
+      labels = [issue["epic"], 'github-import']
+      story_hash = {:name => issue["title"],
+        :description => issue["description"],
+        :labels => labels,
+        :story_type => story_type,
+        :current_state => story_current_state
+      }
+      puts "story hash #{story_hash}"
+
+      unless @dry_run
+        story = @pivotal_project.stories.create(story_hash)
+
+        puts "created #{story.url}"
+
+        # url given is to the API endpoint to get the story
+        # transform it here.
+        issue_url = issue["url"].sub(/api\./, '').sub(/\/repos/, '')
+
+        story.notes.create(
+                           text: "Migrated from #{issue_url}",
+                           )
+        puts "created comment for #{story.url}"
+
+        ### LE: don't import comments
+
+        # comments.each do |comment|
+        #   story.notes.create(
+        #     text: comment.body.gsub(/\r\n\r\n/, "\n\n"),
+        #     author: comment.user.login,
+        #     noted_at: comment.created_at
+        #   )
+        # end
+
+        @github.add_comment(GITHUB_REPO, issue["id"], "Migrated to pivotal tracker #{story.url}")
+
+        ### LE: Don't close issue
+
+        # github.close_issue(GITHUB_REPO, issue.number)
+
+      end
+    end
+  rescue StandardError => se
+    puts se.message
+    exit 1
+  end
+
+
+end
+
+# uncomment to debug
+# require 'net-http-spy'
+# puts ENV['GITHUB_TOKEN']
+# puts ENV['PIVOTAL_TOKEN']
+
+@dry_run = false
+
+@github = Octokit::Client.new(:access_token => GITHUB_TOKEN)
+
+PivotalTracker::Client.token = PIVOTAL_TOKEN
+PivotalTracker::Client.use_ssl = PIVOTAL_PROJECT_USE_SSL
+
+@pivotal_project = PivotalTracker::Project.find(PIVOTAL_PROJECT_ID)
+
+###########################
+## export issues to csv: ##
+###########################
+
+# github_issues_to_csv("blah_test.csv")
+
+###################
+## import issues ##
+###################
+
+begin
+  issues = fix_up_epics(get_csv_arr("edited.csv"))
+  pivotal_import_stories(issues)
+rescue Exception => e
+  puts e.backtrace
 end
